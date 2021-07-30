cbb37a684de5a5f2c821bc4166353d168448fc5c
Luis Rangel <luisrangelc@gmail.com>
2021-07-29 21:45:06 -0600

    Bulma css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,169 +1,179 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-import React, { useState, useEffect } from 'react';
-
-const ArrowFunction = () => {
-  console.log('logging from arrow function');
-  return (
-    <>
-      <h1>I am arrow function</h1>
-    </>
-  );
-};
-
-function CompA(props) {
-  useEffect(() => {
-    console.log('CompoA useEffect props.myProp1');
-  }, [props.myProp1]);
-
-  return (
-    <>
-      <ArrowFunction />
-      <h1>CompA</h1>
-      <p>Hello Comp A</p>
-      <div>My Prop1: {props.myProp1}</div>
-      <div>My Prop2: {props.myProp2}</div>
-      <div>My Prop3: {props.myProp3.toString()}</div>
-      <div>My Prop4: {<props.myProp4 />}</div>
-      <CompB />
-    </>
-  );
-
-  // return React.createElement(
-  //   'div',
-  //   null,
-  //   React.createElement('h1', null, 'CompA'),
-  //   React.createElement('p', null, 'Hello Comp A'),
-  //   React.createElement(CompB)
-  // );
-}
-
-function CompB() {
-  return (
-    <>
-      <h1>CompB</h1>
-      <p>Hello Comp B</p>
-    </>
-  );
-
-  // return (
-  //   <React.Fragment>
-  //     <h1>CompB</h1>
-  //     <p>Hello Comp B</p>
-  //   </React.Fragment>
-  // );
-}
-
-// JSX - Javascript XML
-class CompC extends React.Component<{ myProp1; MyProp2 }, { [key: string]: any }> {
-  // state = {
-  //   myValue: 10,
-  // };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      myValue: 10,
-    };
-  }
-
-  changeState(incrementor) {
-    this.setState({
-      myValue: incrementor,
-    });
-    console.log(incrementor);
-  }
-
-  render() {
-    // const myValue = this.state.myValue;
-    const { myValue } = this.state;
-    const { myProp1, MyProp2 } = this.props;
-
-    console.log(`I am re-executed`);
-
-    return (
-      <>
-        <h1>Hello CompC</h1>
-        Current Value: <h1>{myValue}</h1>
-        <button onClick={() => this.changeState(myValue + 1)}>+</button>
-        <button onClick={() => this.changeState(myValue - 1)}>-</button>
-        <h2>Props myProp1: {this.props.myProp1}</h2>
-        <h2>Props myProp1: {myProp1}</h2>
-        <MyProp2 />
-      </>
-    );
-  }
-}
-
-class CompD extends React.Component {
-  render() {
-    return React.createElement('h1', null, 'Hello CompD');
-  }
-}
-
-function MyComponent() {
-  return <h1>My Component!</h1>;
-}
+import Image from 'next/image';
+// import logoPic from '../public/images/bulma.png';
+// import firstPostPic from '../public/images/first-post.png';
+const logoPic = '/images/bulma.png';
+const firstPostPic = '/images/first-post.png';
 
 function Home() {
-  // [stateValue, mutateState]
-  // const valueState = useState(10);
-  // const value = valueState[0];
-  // const setValue = valueState[1];
-
-  // console.log('valueState');
-  // console.log(valueState);
-  // console.log('valueState[0]');
-  // console.log(valueState[0]);
-
-  const [myValue, setValue] = useState(10);
-  const [myOtherValue, setOtherValue] = useState(100);
-
-  const incrementValue = () => {
-    console.log(`myValue: ${myValue}`);
-    setValue(myValue + 1);
-    // setValue(myValue++);
-    console.log(`myValue + 1: ${myValue}`);
-  };
-
-  const decrementValue = () => {
-    console.log(`myValue: ${myValue}`);
-    setValue(myValue - 1);
-    // setValue(myValue--);
-    console.log(`myValue - 1: ${myValue}`);
-  };
-
-  console.log(`myValueX: ${myValue}`);
-
-  const changeValue = (incrementor) => {
-    setValue(myValue + incrementor);
-  };
-
-  useEffect(() => {
-    console.log('Use Effect called! myOtherValue');
-  }, [myOtherValue]);
-
-  useEffect(() => {
-    console.log('Use Effect called! myValue');
-  }, [myValue]);
-
   return (
     <>
-      Current Value: <h1>{myValue}</h1>
-      <button onClick={incrementValue}>+</button>
-      <button onClick={decrementValue}>-</button>
-      <hr />
-      Other Value: <h1>{myOtherValue}</h1>
-      <button onClick={() => setOtherValue(myOtherValue + 1)}>+</button>
-      <button onClick={() => setOtherValue(myOtherValue - 1)}>-</button>
-      <CompB />
-      <button onClick={() => changeValue(+1)}>+</button>
-      <button onClick={() => changeValue(-1)}>-</button>
-      <h1>Hello World! HomePage</h1>
-      <CompA myProp1={myValue} myProp2='My Custom Value' myProp3={true} myProp4={() => <div>My NEW JSX!</div>} />
-      <CompC myProp1={myValue} MyProp2={MyComponent} />
-      <CompD />
+      <nav className='navbar'>
+        <div className='container'>
+          <div className='navbar-brand'>
+            <a className='navbar-item' href='../'>
+              <Image priority src={logoPic} alt='Logo' width={100} height={100} />
+            </a>
+            <span className='navbar-burger burger' data-target='navbarMenu'>
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id='navbarMenu' className='navbar-menu'>
+            <div className='navbar-end'>
+              <div className=' navbar-item'>
+                <div className='control has-icons-left'>
+                  <input className='input is-rounded' type='email' placeholder='Search' />
+                  <span className='icon is-left'>
+                    <i className='fa fa-search'></i>
+                  </span>
+                </div>
+              </div>
+              <a className='navbar-item is-active is-size-5 has-text-weight-semibold'>Home</a>
+              <a className='navbar-item is-size-5 has-text-weight-semibold'>Examples</a>
+              <a className='navbar-item is-size-5 has-text-weight-semibold'>Features</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <section className='hero '>
+        <div className='hero-body'>
+          <div className='container'>
+            <div className='columns'>
+              <div className='column is-8 is-offset-2'>
+                <figure className='image is-16by9'>
+                  <Image priority src={firstPostPic} alt='Logo' width={100} height={100} />
+                </figure>
+              </div>
+            </div>
+
+            <section className='section'>
+              <div className='columns'>
+                <div className='column is-8 is-offset-2'>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-4'>December 25, 2018</h2>
+                    <h1 className='title'>Getting Started</h1>
+                    <p>
+                      This is a starter template for creating a beautiful, customizable blog with minimal effort. You’ll only have to change
+                      a few settings and you’re ready to go. As with all Jigsaw sites, configuration settings can be found in config
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <div className='is-divider'></div>
+
+            <section className='section'>
+              <div className='columns'>
+                <div className='column is-8 is-offset-2'>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-4'>December 25, 2018</h2>
+                    <h1 className='title'>Getting Started</h1>
+                    <p>
+                      This is a starter template for creating a beautiful, customizable blog with minimal effort. You’ll only have to change
+                      a few settings and you’re ready to go. As with all Jigsaw sites, configuration settings can be found in config
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+
+      <section className='section'>
+        <div className='columns'>
+          <div className='column is-10 is-offset-1'>
+            <div className='container has-text-centered is-fluid'>
+              <div className='hero is-light'>
+                <div className='hero-body'>
+                  <h2 className='title is-4'>Sign up for our newsletter</h2>
+                  <div className='column is-6 is-offset-3'>
+                    <div className='field has-addons has-addons-centered'>
+                      <div className='control is-expanded'>
+                        <input className='input ' type='text' placeholder='Email address' />
+                      </div>
+                      <div className='control'>
+                        <a className='button is-info'>Subscribe</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='hero '>
+        <div className='hero-body'>
+          <div className='container'>
+            <section className='section'>
+              <div className='columns is-variable is-8'>
+                <div className='column is-5 is-offset-1 '>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-5 has-text-grey'>December 23, 2018</h2>
+                    <h1 className='title has-text-black is-3'>Custom 404 Pages</h1>
+                    <p className='has-text-dark'>
+                      This starter template includes a custom 404 Not Found error page, located at /source/404.blade.php. To preview the 404
+                      page, you can visit /404 in your browser. Depending...
+                    </p>
+                  </div>
+                </div>
+                <div className='column is-5'>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-5 has-text-grey'>December 25, 2018</h2>
+                    <h1 className='title has-text-black is-3'>Fuse Search</h1>
+                    <p className='has-text-dark'>
+                      To provide fast, local search of your blog, this starter template comes with a pre-built Vue.js component that uses
+                      Fuse.js. Fuse.js is a lightweight fuzzy-search library with no...
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <div className='is-divider'></div>
+
+            <section className='section'>
+              <div className='columns is-variable is-8'>
+                <div className='column is-5 is-offset-1'>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-5 has-text-grey'>December 25, 2018</h2>
+                    <h1 className='title has-text-black is-3'>Getting Started</h1>
+                    <p className='has-text-dark'>
+                      This is a starter template for creating a beautiful, customizable blog with minimal effort. You’ll only have to change
+                      a few settings and you’re ready to go. As with all Jigsaw sites, configuration settings can be found in config
+                    </p>
+                  </div>
+                </div>
+                <div className='column is-5'>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-5 has-text-grey'>December 25, 2018</h2>
+                    <h1 className='title has-text-black is-3'>Getting Started</h1>
+                    <p className='has-text-dark'>
+                      This is a starter template for creating a beautiful, customizable blog with minimal effort. You’ll only have to change
+                      a few settings and you’re ready to go. As with all Jigsaw sites, configuration settings can be found in config
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+
+      <footer className='footer'>
+        <div className='content has-text-centered'>
+          <p>
+            <strong>Bulma - Blog theme</strong> by <a href='https://gonzalojs.com'>Gonzalo Gutierrez</a>. Based on the{' '}
+            <a href='http://jigsaw-blog-staging.tighten.co/'>jigsaw-blog</a>. The source code is licensed
+            <a href='http://opensource.org/licenses/mit-license.php'>MIT</a>
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
