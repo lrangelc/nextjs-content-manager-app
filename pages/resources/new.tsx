39b00bd652f8a3ca27161f7a0063bc08b0362338
Luis Rangel <luisrangelc@gmail.com>
2021-08-05 21:50:48 -0600

    Resource form

--- pages/resources/new.tsx
+++ pages/resources/new.tsx
@@ -3,7 +3,32 @@ import Layout from '../../components/Layout';
 const ResourceCreate = () => {
   return (
     <Layout>
-      <div>I am resource create page</div>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column is-8 is-offset-2'>
+            <div className='resource-form'>
+              <h1 className='title'>Add New Resource</h1>
+              <form>
+                <div className='field'>
+                  <label className='label'>Name</label>
+                  <div className='control'>
+                    <input className='input' type='text' placeholder='Text input' />
+                  </div>
+                </div>
+
+                <div className='field is-grouped'>
+                  <div className='control'>
+                    <button className='button is-link'>Submit</button>
+                  </div>
+                  <div className='control'>
+                    <button className='button is-link is-light'>Cancel</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
