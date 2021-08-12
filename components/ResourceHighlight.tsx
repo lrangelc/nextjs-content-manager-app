551218af418f53d7d2737e974389ed7f84ae3bac
Luis Rangel <luisrangelc@gmail.com>
2021-08-11 21:18:34 -0600

    Resource detail page

--- components/ResourceHighlight.tsx
+++ components/ResourceHighlight.tsx
@@ -4,34 +4,32 @@ const firstPostPic = '/images/first-post.png';
 
 const ResourceHightlight = (props) => {
   return (
-    <>
-      <section className='hero'>
-        <div className='hero-body'>
-          <div className='container'>
-            <div className='columns'>
-              <div className='column is-8 is-offset-2'>
-                <Image priority src={firstPostPic} alt='Logo' width={300} height={300} />
-              </div>
+    <section className='hero'>
+      <div className='hero-body'>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column is-8 is-offset-2'>
+              <Image priority src={firstPostPic} alt='Logo' width={300} height={300} />
             </div>
-            {props.resources.map((resource) => {
-              return (
-                <section className='section' key={resource.id}>
-                  <div className='columns'>
-                    <div className='column is-8 is-offset-2'>
-                      <div className='content is-medium'>
-                        <h2 className='subtitle is-4'>{resource.createdAt}</h2>
-                        <h1 className='title'>{resource.title}</h1>
-                        <p>{resource.description}</p>
-                      </div>
+          </div>
+          {props.resources.map((resource) => {
+            return (
+              <section className='section' key={resource.id}>
+                <div className='columns'>
+                  <div className='column is-8 is-offset-2'>
+                    <div className='content is-medium'>
+                      <h2 className='subtitle is-4'>{resource.createdAt}</h2>
+                      <h1 className='title'>{resource.title}</h1>
+                      <p>{resource.description}</p>
                     </div>
                   </div>
-                </section>
-              );
-            })}
-          </div>
+                </div>
+              </section>
+            );
+          })}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
---
+++ pages/resources/[id].tsx
@@ -0,0 +1,28 @@
+import Layout from '../../components/Layout';
+
+const ResourceDetail = () => {
+  const resource = { id: '', createdAt: '', title: '', description: '' };
+  return (
+    <Layout>
+      <section className='hero'>
+        <div className='hero-body'>
+          <div className='container'>
+            <section className='section' key={resource.id}>
+              <div className='columns'>
+                <div className='column is-8 is-offset-2'>
+                  <div className='content is-medium'>
+                    <h2 className='subtitle is-4'>{resource.createdAt}</h2>
+                    <h1 className='title'>{resource.title}</h1>
+                    <p>{resource.description}</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default ResourceDetail;
